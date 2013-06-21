905f4396f3a69dc2d8ec0877b49402ccc15787d7
dpismenny <denis.pismenny@gmail.com>
2013-06-21 16:37:37 +0300

    fix layout and fonts

--- js/common.js
+++ js/common.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
 
+	// ---------- dropdown -------------------- //
+		jQuery('.js-dropdown').click(function(event) {
+			$(this).children('ul').slideToggle();
+			$(this).toggleClass('is-open');
+		});
+
 });
