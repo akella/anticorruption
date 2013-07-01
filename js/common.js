a3bf50a61d16b2d8a8e35f2859f6733d3746ff27
dpismenny <denis.pismenny@gmail.com>
2013-07-01 14:38:45 +0300

    Issue #2

--- js/common.js
+++ js/common.js
@@ -11,9 +11,27 @@ $(document).ready(function() {
 			$('.overlay,.popup').toggleClass('is-open');
 		});
 
-	// ---------- Popup with overlay -------------------- //
+	// ---------- Search erase button -------------------- //
 		$('.js-search-erase').click(function(){
 			$('.js-search-text').val('');
+		});
+
+	// ---------- js-search-filters is-active -------------------- //
+		var select_all = $('.js-select-all');
+		var select_all_value = select_all.attr('data-search');
+
+		$('.js-search-filters span').click(function(){
+			if ($(this).parent().hasClass('is-active')){
+				$(this).parent().removeClass('is-active');
+				$('.js-select-all').parent().addClass('is-active');
+				$('.js-search-text').attr('data-filter', select_all_value);
+			}
+			else {
+				var search = $(this).attr('data-search');
+				$('.js-search-text').attr('data-filter', search);
+				$('.js-search-filters li').removeClass('is-active');
+				$(this).parent().addClass('is-active');
+			}
 		});
 
 	// ---------- search -------------------- //
